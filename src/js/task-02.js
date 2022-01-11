a4044bf88b3679d76db856970194e663986a4c73
Andrey-Baranyuk <andreybaranyuk86@gmail.com>
2022-01-11 09:56:28 +0200

    02 task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const allIngredients = document.querySelector("ul#ingredients");
+
+const addIngredient = ingredients.map( option => {
+       const createingredient = document.createElement("li");
+       createingredient.textContent = option;
+       return createingredient;
+});
+console.log(addIngredient);
+allIngredients.append(...addIngredient);
