87fc9a384d7f743f1f62c01b0a65a2bb9d63acc3
Andrey-Baranyuk <andreybaranyuk86@gmail.com>
2022-01-13 15:47:49 +0200

    Create task 8 js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const formElements = e.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = { mail, password };
+
+    console.log(formData)
+       
+    if (mail === "" || password === "") {
+
+        return alert(`Все поля должны быть заполнены!`)
+    }
+       
+    
+}
 
